bae3a6a8c7ce0faf1e50e6806917e36bab9faca5
Houcem Hedhly <hedhoucem@gmail.com>
2021-10-11 16:08:30 +0100

    variables

---
+++ 01-les-bases-du-php/02-variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les bases du PHP</title>
+</head>
+
+<body>
+    <h2>Les variables en PHP</h2>
+    <?php
+    // Déclaration d'une variable
+    $a = 1;
+    $pi = 3.14;
+    $msg = "DSI21";
+    echo $a;
+    echo '<br>';
+    echo $pi;
+    echo "<p>$msg</p>";
+    ?>
+</body>
+
+</html>
