276f17821a93ee80d6d12c8743bd0c20f968da8f
Houcem Hedhly <hedhoucem@gmail.com>
2021-10-25 16:51:07 +0100

    paramètrage du formulaire

--- 04-formulaires/exemple1/index.php
+++ 04-formulaires/exemple1/index.php
@@ -11,14 +11,14 @@
 
 <body>
     <div class="container py-4">
-        <form>
+        <form action="traitement.php" method="post">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1">
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
