f30af4bcd0a1d70273af4b45f27447f4e5900ccb
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-15 16:45:11 +0100

    create a layout page

--- 05-connect-bd/index.php
+++ 05-connect-bd/index.php
@@ -1,8 +1,10 @@
 <?php
     include './dbconnect.php';
-    
+
     $query = $pdo->query("SELECT * FROM todos ORDER BY complete, due_date");
     $todos = $query->fetchAll();
 
-    include './index.phtml';
+    $template = 'index';
+    $pageTitle = 'Home page';
+    include './layout.phtml';
     
--- 05-connect-bd/todo_details.php
+++ 05-connect-bd/todo_details.php
@@ -3,5 +3,7 @@
     $query = $pdo->query('SELECT * FROM todos WHERE id='.$_GET['id']);
     $todo = $query->fetch();
 
-    include './todo_details.phtml';
+    $template = 'todo_details';
+    $pageTitle = 'Todo details';
+    include './layout.phtml';
     
