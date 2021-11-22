1559740b0a7dc9c19e51a0ea808102cca9a7d8fe
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-22 15:56:13 +0100

    add new todo logic

--- 05-connect-bd/add_todo.php
+++ 05-connect-bd/add_todo.php
@@ -1,5 +1,20 @@
 <?php
 
+include './dbconnect.php';
+
+if (!empty($_POST)) {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $due_date = $_POST['due_date'];
+
+    $sql = "INSERT INTO todos (title, description, due_date) VALUES (?, ?, ?)";
+    $query = $pdo->prepare($sql);
+    $query->execute([$title, $description, $due_date]);
+
+    header('Location: index.php');
+    exit;
+}
+
 $template = 'add_todo';
 $pageTitle = 'Add todo page';
 include './layout.phtml';
