82bc06931c430933849fd8d9dd0e2c5737269e0a
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-22 16:07:17 +0100

    sanitize submitted data

--- 05-connect-bd/add_todo.php
+++ 05-connect-bd/add_todo.php
@@ -1,11 +1,12 @@
 <?php
 
 include './dbconnect.php';
+include './utilities.php';
 
 if (!empty($_POST)) {
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $due_date = $_POST['due_date'];
+    $title = checkData($_POST['title']);
+    $description = checkData($_POST['description']);
+    $due_date = checkData($_POST['due_date']);
 
     $sql = "INSERT INTO todos (title, description, due_date) VALUES (?, ?, ?)";
     $query = $pdo->prepare($sql);
