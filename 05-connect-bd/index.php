c1b5c9fb728a53ce326bf02ddcddbde40310c5ea
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-22 15:24:39 +0100

    check the id is a valid id

--- 05-connect-bd/index.php
+++ 05-connect-bd/index.php
@@ -1,11 +1,10 @@
 <?php
-    include './dbconnect.php';
+include './dbconnect.php';
 
-    $query = $pdo->prepare("SELECT * FROM todos ORDER BY complete, due_date");
-    $query->execute();
-    $todos = $query->fetchAll();
+$query = $pdo->prepare("SELECT * FROM todos ORDER BY complete, due_date");
+$query->execute();
+$todos = $query->fetchAll();
 
-    $template = 'index';
-    $pageTitle = 'Home page';
-    include './layout.phtml';
-    
+$template = 'index';
+$pageTitle = 'Home page';
+include './layout.phtml';
--- 05-connect-bd/todo_details.php
+++ 05-connect-bd/todo_details.php
@@ -1,10 +1,19 @@
 <?php
-    include './dbconnect.php';
-    $query = $pdo->prepare('SELECT * FROM todos WHERE id= ?');
-    $query->execute([$_GET['id']]);
-    $todo = $query->fetch();
+if (!array_key_exists('id', $_GET) or !ctype_digit($_GET['id'])) {
+    header('Location: index.php');
+    exit();
+}
 
-    $template = 'todo_details';
-    $pageTitle = 'Todo details';
-    include './layout.phtml';
-    
+include './dbconnect.php';
+$query = $pdo->prepare('SELECT * FROM todos WHERE id= ?');
+$query->execute([$_GET['id']]);
+$todo = $query->fetch();
+
+if (!$todo) {
+    header('Location: index.php');
+    exit();
+}
+
+$template = 'todo_details';
+$pageTitle = 'Todo details';
+include './layout.phtml';
