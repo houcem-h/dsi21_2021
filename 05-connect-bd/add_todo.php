f7d988ac88df217cf927fed4b0833d7df148bdf1
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-22 16:24:02 +0100

    display validation errors

--- 05-connect-bd/add_todo.php
+++ 05-connect-bd/add_todo.php
@@ -3,18 +3,21 @@
 include './dbconnect.php';
 include './utilities.php';
 
+$errors = [];
+
 if (!empty($_POST)) {
     $title = checkData($_POST['title']);
     $description = checkData($_POST['description']);
     $due_date = checkData($_POST['due_date']);
-
-    $errors = [];
 
     if (empty($title)) {
         $errors['title'] = 'Title is required';
     }
     if (empty($description)) {
         $errors['description'] = 'Description is required';
+    }
+    if (empty($due_date)) {
+        $errors['due_date'] = 'Due date is required';
     }
 
     if (empty($errors)) {
