0d717323e57ad81ebf80c1e4c89ebdc9e5e967cf
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-06 16:49:29 +0100

    create new todo method

--- 06-todo-app-poo/classes/Todo.php
+++ 06-todo-app-poo/classes/Todo.php
@@ -35,4 +35,19 @@ class Todo
         $query = $this->pdo->launchQuery($sql, ['id' => $id]);
         return $query->fetch();
     }
+
+    /**
+     * Create a new todo
+     *
+     * @param string $title
+     * @param date $due_date
+     * @param string $description
+     * @return int $id
+     */
+    public function create($title, $due_date, $description)
+    {
+        $sql = "INSERT INTO todos (title, description, due_date, user_id) VALUES (?, ?, ?, ?)";
+        $this->pdo->launchQuery($sql, [$title, $description, $due_date, 1]);
+        return $this->pdo->lastInsertId();
+    }
 }
