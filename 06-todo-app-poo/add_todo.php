9f009f985891e5e545ead4f28f63a68d71333b6d
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-24 22:22:03 +0100

    redirect unauthenticated user

--- 06-todo-app-poo/add_todo.php
+++ 06-todo-app-poo/add_todo.php
@@ -1,5 +1,11 @@
 <?php
 
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: ./login.php');
+    exit();
+}
+
 $errors = [];
 $title = '';
 $description = '';
--- 06-todo-app-poo/index.php
+++ 06-todo-app-poo/index.php
@@ -1,5 +1,11 @@
 <?php
 
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: ./login.php');
+    exit();
+}
+
 include './classes/Todo.php';
 
 $todo = new Todo();
